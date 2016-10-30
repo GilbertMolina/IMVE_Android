60897e405e7a2fee771734a0592c328ef3ae6692
Gilberth Molina <gmolinac@outlook.com>
2016-10-30 14:58:37 -0600

    Se agregan nuevamente los archivos del proyecto actualizados
    Se agregan nuevamente los archivos del proyecto actualizados

---
+++ SistemaIMVE/app/src/main/java/com/gmolina/sistemaimve/MainActivity.java
@@ -0,0 +1,80 @@
+package com.gmolina.sistemaimve;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar progressBar;
+    private LinearLayout layoutProgress;
+    private String url = "http://sistemaimve.tigrimigri.com/IMVE/index.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        layoutProgress = (LinearLayout) findViewById(R.id.layoutProgress);
+
+        webView.setVisibility(View.GONE);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setSupportZoom(true);
+        settings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.setVisibility(View.VISIBLE);
+                layoutProgress.setVisibility(View.GONE);
+                progressBar.setIndeterminate(false);
+                super.onPageFinished(view, url);
+
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                layoutProgress.setVisibility(View.VISIBLE);
+                progressBar.setIndeterminate(true);
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+
+        if(isOnline()) {
+            webView.loadUrl(url);
+        } else {
+            setContentView(R.layout.activity_sin_conexion);
+            toast("No hay conexión a internet");
+        }
+    }
+
+    private void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+    }
+}
